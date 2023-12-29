22010ef5b99f84ba3536c051557c6ebd2f3c2c24
sahil77755 <150704201+sahil77755@users.noreply.github.com>
2023-12-28 22:36:08 -0800

    Add files via upload

---
+++ String formatting.py
@@ -0,0 +1,15 @@
+name = "talha"
+age = 18
+print("hello {} enter your age {}".format(name,age))
+var , aer = "good" , 77
+print("Enter  is name {} tell me your age {}".format(var ,aer))
+gst , had = "where are you good job", 88
+print('hello {} world {}'.format(gst, had))
+yahoo , baba = "", 4
+print("name {} agr {}".format(yahoo , baba))
+ses , los = 77, 56
+print('guuk {} cvd{}'.format(ses ,los))
+htg , kok = "kaunger" , 88
+print('nahu {} naj {}'.format(htg ,kok))
+ij , hy = "ali", "kkkk"
+print('hamza {} kij {}'.format(ij, hy))
